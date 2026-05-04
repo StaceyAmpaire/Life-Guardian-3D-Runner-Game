3b0a3080f7e1f14d8a59bb410eda4312900df3c5
staceyampaire3 <staceyampaire3@gmail.com>
2026-05-04 13:41:39 +0300

    Fixed Tutorial Panel

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,42 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject tutorialPanel;
-
-    // Static variables persist as long as the game is running, even if the scene changes
-    private static bool tutorialShownThisSession = false;
+    public TutorialManager tutorialManager; // Reference to the new TutorialManager script
 
     void Start()
     {
-        // DEVELOPER MODE: Shows every time you launch the game, but not when returning to menu
-        if (!tutorialShownThisSession)
+        // Check if the tutorial has been shown this session using MasterInfo's flag
+        if (!MasterInfo.tutorialShownThisSession)
         {
-            tutorialPanel.SetActive(true);
-            tutorialShownThisSession = true;
+            tutorialManager.ShowTutorial();
+            MasterInfo.tutorialShownThisSession = true; // Set the flag so it doesn't show again this session
         }
         else
         {
-            tutorialPanel.SetActive(false);
+            // Ensure the tutorial panel is hidden if it has already been shown this session
+            tutorialManager.tutorialPanel.SetActive(false);
         }
-
-        /* 
-        // PLAYER MODE: Uncomment this and delete the "DEVELOPER MODE" block above 
-        // to make it show only once ever for the player.
-        
-        if (PlayerPrefs.GetInt("HasSeenTutorial", 0) == 0)
-        {
-            tutorialPanel.SetActive(true);
-            PlayerPrefs.SetInt("HasSeenTutorial", 1);
-            PlayerPrefs.Save();
-        }
-        else
-        {
-            tutorialPanel.SetActive(false);
-        }
-        */
     }
 
-    public void OpenTutorial() => tutorialPanel.SetActive(true);
-    public void CloseTutorial() => tutorialPanel.SetActive(false);
+    // This method will be called by a UI button to manually open the tutorial
+    public void OpenTutorial()
+    {
+        tutorialManager.ShowTutorial();
+    }
+
+    // This method will be called by the 'I Understand' button in the TutorialManager
+    // No longer needed here as TutorialManager handles closing itself.
+    // public void CloseTutorial() => tutorialManager.tutorialPanel.SetActive(false);
 }
