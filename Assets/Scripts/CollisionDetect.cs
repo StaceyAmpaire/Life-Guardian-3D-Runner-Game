061ddf64d7fc46063c752aa6d3f7b77f9401cef3
staceyampaire3 <staceyampaire3@gmail.com>
2026-06-10 22:36:49 +0300

    Added Player Obstacle jump in pc+Android build, Obstacle spawner

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -2,33 +2,46 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-
 public class CollisionDetect : MonoBehaviour
 {
-   [SerializeField] GameObject thePlayer;
-   [SerializeField] GameObject playerAnim;
-   [SerializeField] AudioSource collisionFX;
-   [SerializeField] GameObject mainCam;
-   [SerializeField] GameObject fadeOut;
+    [SerializeField] GameObject thePlayer;
+    [SerializeField] GameObject playerAnim;
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeOut;
+
+    void Start()
+{
+    
+    thePlayer = GameObject.Find("Guardian");
+    playerAnim = GameObject.Find("Running (1)");
+    mainCam = GameObject.Find("Main Camera");
+    fadeOut = GameObject.Find("FadeOut");
+
+    collisionFX = GameObject.Find("CollisionFX")
+                            .GetComponent<AudioSource>();
+}
+
     void OnTriggerEnter(Collider other)
     {
         StartCoroutine(CollisionEnd());
-      
-}
-
-IEnumerator CollisionEnd()
-    {
-         collisionFX.Play();
-       thePlayer.GetComponent<PlayerMovement>().enabled = false;
-       //it will stop moving and give us room to play the animation
-       playerAnim.GetComponent<Animator>().Play("Stumblerunner1");
-       mainCam.GetComponent<Animator>().Play("CollisionCam");
-       yield return new WaitForSeconds(3);
-       fadeOut.SetActive(true);
-       yield return new WaitForSeconds(3);
-         FindFirstObjectByType<EndRunUI>().ShowPopup();
-         
-
     }
 
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
+        thePlayer.GetComponent<PlayerMovement>().enabled = false;
+        playerAnim.GetComponent<Animator>().Play("Stumblerunner1");
+        mainCam.GetComponent<Animator>().Play("CollisionCam");
+
+        yield return new WaitForSeconds(2);
+
+//Debug.Log("Fade Trigger Fired");
+fadeOut.GetComponent<Animator>()
+       .SetTrigger("Fade");
+
+yield return new WaitForSeconds(3);
+
+FindFirstObjectByType<EndRunUI>().ShowPopup();
+    }
 }
